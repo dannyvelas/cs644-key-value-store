6a41fafd2e43766afd4cfb743028329530e70247
Daniel Velasquez Neira <danivelas4@gmail.com>
2025-10-28 17:22:06 -0400

    v1 of signal logic M src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,23 @@ mod handler;
 mod net;
 mod store;
 
-fn get_sigfd(set: &libc::sigset_t) -> Result<os::raw::c_int, io::Error> {
+fn signal() -> Result<(), io::Error> {
     #[cfg(target_os = "linux")]
-    {
-        let sigfd = unsafe { libc::signalfd(-1, set, 0) };
-        if sigfd == -1 {
-            return Err(io::Error::last_os_error());
-        }
-        Ok(sigfd)
-    }
-
-    #[cfg(not(target_os = "linux"))]
-    Ok(34)
-}
-
-fn main() -> Result<(), Box<dyn error::Error>> {
-    // signal stuff
     unsafe {
-        let mut set: libc::sigset_t = mem::zeroed();
         // define set of SIGUSR1 and mask it
+        let mut set: libc::sigset_t = mem::zeroed();
         libc::sigemptyset(&mut set);
         libc::sigaddset(&mut set, libc::SIGUSR1);
         if libc::sigprocmask(libc::SIG_BLOCK, &set, ptr::null_mut()) == -1 {
             return Err(io::Error::last_os_error().into());
         }
+
         // get a new file descriptor that will be used to read signals
-        let sigfd = get_sigfd(&set)?;
+        let sigfd = libc::signalfd(-1, &set, 0);
+        if sigfd == -1 {
+            return Err(io::Error::last_os_error());
+        }
+
         // define set of descriptors that we can listen to
         let mut readfds: libc::fd_set = mem::zeroed();
         libc::FD_ZERO(&mut readfds);
@@ -64,7 +55,16 @@ fn main() -> Result<(), Box<dyn error::Error>> {
                 _ => println!("received {} signal", info.ssi_signo),
             }
         }
+        Ok(())
     }
+
+    #[cfg(not(target_os = "linux"))]
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    // signal stuff
+    signal()?;
 
     // define deps
     let disk_map = store::DiskMap::new("/tmp/map")?;

ec0efcef00748fbb7f6afd0e8388eef5ae9ce058
Daniel Velasquez Neira <danivelas4@gmail.com>
2025-11-03 18:35:45 -0500

    re-ordered stuff M src/net/server.rs

--- src/net/server.rs
+++ src/net/server.rs
@@ -82,13 +82,10 @@ impl TCPServer {
         signal_fd: i32,
         sock_fd: i32,
     ) -> Result<(), Box<dyn error::Error>> {
-        let fd = event.u64 as i32;
-        if fd == signal_fd {
-            self.accept_signal(signal_fd)
-        } else if fd == sock_fd {
-            self.accept_conn(sock_fd)
-        } else {
-            Err(format!("received unexpected event of fd: {}", fd).into())
+        match event.u64 as i32 {
+            fd if fd == signal_fd => self.accept_signal(signal_fd),
+            fd if fd == sock_fd => self.accept_conn(sock_fd),
+            fd => Err(format!("received unexpected event of fd: {}", fd).into()),
         }
     }
 
@@ -101,13 +98,15 @@ impl TCPServer {
         } else if read == 0 {
             return Err("read 0 bytes from signal pipe...somehow".into());
         };
-        match buf[0] {
-            2 => Err("received SIGINT".into()),
-            signal => {
-                println!("received {}", signal);
-                Ok(())
-            }
+
+        let signal = buf[0] as i32;
+        if signal == libc::SIGINT {
+            return Err("received SIGINT".into());
+        } else if signal == libc::SIGUSR1 {
+            self.handler.handle("compact")
         }
+
+        return Ok(());
     }
 
     fn accept_conn(&self, sock_fd: i32) -> Result<(), Box<dyn error::Error>> {
@@ -128,97 +127,6 @@ impl TCPServer {
             libc::EINVAL => Err("invalid settings in attr".into()),
             libc::EPERM => Err("insufficient permissions".into()),
             ret => Err(format!("unexpected return value: {}", ret).into()),
-        }
-    }
-
-    fn setup_epoll(
-        epoll_fd: i32,
-        signal_fd: i32,
-        sock_fd: i32,
-    ) -> Result<(), Box<dyn error::Error>> {
-        unsafe {
-            let mut signal_ev = libc::epoll_event {
-                events: libc::EPOLLIN as u32,
-                u64: signal_fd as u64,
-            };
-            if libc::epoll_ctl(epoll_fd, libc::EPOLL_CTL_ADD, signal_fd, &mut signal_ev) == -1 {
-                return Err(format!(
-                    "error adding signal to epoll: {}",
-                    io::Error::last_os_error()
-                )
-                .into());
-            }
-
-            let mut sock_ev = libc::epoll_event {
-                events: libc::EPOLLIN as u32,
-                u64: sock_fd as u64,
-            };
-            if libc::epoll_ctl(epoll_fd, libc::EPOLL_CTL_ADD, sock_fd, &mut sock_ev) == -1 {
-                return Err(format!(
-                    "error adding socket to epoll: {}",
-                    io::Error::last_os_error()
-                )
-                .into());
-            }
-        }
-        Ok(())
-    }
-
-    fn local_sockfd(port: &str) -> Result<i32, Box<dyn error::Error>> {
-        let hints = libc::addrinfo {
-            ai_flags: 0,
-            ai_family: libc::AF_INET,
-            ai_socktype: libc::SOCK_STREAM,
-            ai_protocol: 0,
-            ai_addrlen: 0,
-            ai_canonname: ptr::null_mut(),
-            ai_addr: ptr::null_mut(),
-            ai_next: ptr::null_mut(),
-        };
-        let mut result = ptr::null_mut();
-        let host = ffi::CString::new("localhost")?;
-        let port = ffi::CString::new(port)?;
-        unsafe {
-            let status = libc::getaddrinfo(
-                host.as_ptr(),
-                port.as_ptr(),
-                &hints as *const libc::addrinfo,
-                &mut result,
-            );
-            if status != 0 {
-                let err_msg_raw = libc::gai_strerror(status);
-                let err_msg = ffi::CStr::from_ptr(err_msg_raw).to_str()?;
-                return Err(format!("error calling getaddrinfo: {}", err_msg).into());
-            }
-
-            let mut result_ptr = result;
-            let mut sock_fd = 0;
-            while !result_ptr.is_null() {
-                sock_fd = libc::socket(
-                    (*result_ptr).ai_family,
-                    (*result_ptr).ai_socktype,
-                    (*result_ptr).ai_protocol,
-                );
-                if sock_fd == -1 {
-                    result_ptr = (*result_ptr).ai_next;
-                    continue;
-                }
-
-                if libc::bind(sock_fd, (*result_ptr).ai_addr, (*result_ptr).ai_addrlen) != 0 {
-                    libc::close(sock_fd);
-                    result_ptr = (*result_ptr).ai_next;
-                    continue;
-                }
-
-                break;
-            }
-            libc::freeaddrinfo(result);
-
-            if result_ptr.is_null() {
-                return Err("could not bind".into());
-            }
-
-            Ok(sock_fd)
         }
     }
 
@@ -290,5 +198,96 @@ impl TCPServer {
             (_, Some(err)) => Err(err),
             (_, None) => Ok(()),
         }
+    }
+
+    fn local_sockfd(port: &str) -> Result<i32, Box<dyn error::Error>> {
+        let hints = libc::addrinfo {
+            ai_flags: 0,
+            ai_family: libc::AF_INET,
+            ai_socktype: libc::SOCK_STREAM,
+            ai_protocol: 0,
+            ai_addrlen: 0,
+            ai_canonname: ptr::null_mut(),
+            ai_addr: ptr::null_mut(),
+            ai_next: ptr::null_mut(),
+        };
+        let mut result = ptr::null_mut();
+        let host = ffi::CString::new("localhost")?;
+        let port = ffi::CString::new(port)?;
+        unsafe {
+            let status = libc::getaddrinfo(
+                host.as_ptr(),
+                port.as_ptr(),
+                &hints as *const libc::addrinfo,
+                &mut result,
+            );
+            if status != 0 {
+                let err_msg_raw = libc::gai_strerror(status);
+                let err_msg = ffi::CStr::from_ptr(err_msg_raw).to_str()?;
+                return Err(format!("error calling getaddrinfo: {}", err_msg).into());
+            }
+
+            let mut result_ptr = result;
+            let mut sock_fd = 0;
+            while !result_ptr.is_null() {
+                sock_fd = libc::socket(
+                    (*result_ptr).ai_family,
+                    (*result_ptr).ai_socktype,
+                    (*result_ptr).ai_protocol,
+                );
+                if sock_fd == -1 {
+                    result_ptr = (*result_ptr).ai_next;
+                    continue;
+                }
+
+                if libc::bind(sock_fd, (*result_ptr).ai_addr, (*result_ptr).ai_addrlen) != 0 {
+                    libc::close(sock_fd);
+                    result_ptr = (*result_ptr).ai_next;
+                    continue;
+                }
+
+                break;
+            }
+            libc::freeaddrinfo(result);
+
+            if result_ptr.is_null() {
+                return Err("could not bind".into());
+            }
+
+            Ok(sock_fd)
+        }
+    }
+
+    fn setup_epoll(
+        epoll_fd: i32,
+        signal_fd: i32,
+        sock_fd: i32,
+    ) -> Result<(), Box<dyn error::Error>> {
+        unsafe {
+            let mut signal_ev = libc::epoll_event {
+                events: libc::EPOLLIN as u32,
+                u64: signal_fd as u64,
+            };
+            if libc::epoll_ctl(epoll_fd, libc::EPOLL_CTL_ADD, signal_fd, &mut signal_ev) == -1 {
+                return Err(format!(
+                    "error adding signal to epoll: {}",
+                    io::Error::last_os_error()
+                )
+                .into());
+            }
+
+            let mut sock_ev = libc::epoll_event {
+                events: libc::EPOLLIN as u32,
+                u64: sock_fd as u64,
+            };
+            if libc::epoll_ctl(epoll_fd, libc::EPOLL_CTL_ADD, sock_fd, &mut sock_ev) == -1 {
+                return Err(format!(
+                    "error adding socket to epoll: {}",
+                    io::Error::last_os_error()
+                )
+                .into());
+            }
+        }
+        Ok(())
     }
 }

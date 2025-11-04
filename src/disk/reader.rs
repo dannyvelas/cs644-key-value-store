45e283ccf543059db611bcea876f455a28339eda
Daniel Velasquez Neira <danivelas4@gmail.com>
2025-11-04 13:55:39 -0500

    cleaned. also fixed bug where dump/get was returning empty when first value was deleted M src/disk/reader.rs

--- src/disk/reader.rs
+++ src/disk/reader.rs
@@ -2,17 +2,53 @@ use std::error;
 
 pub struct Entry {
     pub offset: usize,
+    pub live: bool,
     pub key: String,
     pub value: String,
+    pub len: usize,
 }
 
 impl Entry {
     pub fn new(key: &str, value: &str) -> Entry {
         Entry {
+            live: true,
             offset: 0,
             key: key.to_owned(),
             value: value.to_owned(),
+            len: 1 + 4 + 4 + key.len() + value.len(),
         }
+    }
+
+    pub fn from_bytes(bytes: &[u8], start: usize) -> Option<Entry> {
+        let mut offset = start;
+
+        // get live byte
+        let live = bytes[offset] == 1;
+        offset += 1;
+
+        // get key size field, it is 4 bytes long and stored in big-endian
+        // if number is 0xCAFEBABE, it is stored as CA FE BA BE
+        let key_size_bytes = &bytes[offset..(offset + 4)];
+        let key_size = Entry::parse_size(key_size_bytes);
+        offset += 4;
+
+        // get value size field, also 4 bytes long and stored in big-endian
+        let value_size_bytes = &bytes[offset..(offset + 4)];
+        let value_size = Entry::parse_size(value_size_bytes);
+        offset += 4;
+
+        let key = str::from_utf8(&bytes[offset..(offset + key_size)]).ok()?;
+        offset += key_size;
+        let value = str::from_utf8(&bytes[offset..(offset + value_size)]).ok()?;
+        offset += value_size;
+
+        Some(Entry {
+            live,
+            offset: start,
+            key: key.to_owned(),
+            value: value.to_owned(),
+            len: offset - start,
+        })
     }
 
     pub fn to_bytes(&self) -> Result<Vec<u8>, Box<dyn error::Error>> {
@@ -67,32 +103,16 @@ impl Iterator for ReadResult {
         while self.offset < self.data.len() {
             let start = self.offset;
 
-            // get is_deleted byte, one byte long
-            let is_deleted = self.data[self.offset] == 0;
-            self.offset += 1;
-
-            // get key size field, it is 4 bytes long and stored in big-endian
-            // if number is 0xCAFEBABE, it is stored as CA FE BA BE
-            let key_size_bytes = &self.data[self.offset..(self.offset + 4)];
-            let key_size = Entry::parse_size(key_size_bytes);
-            self.offset += 4;
-
-            // get value size field, also 4 bytes long and stored in big-endian
-            let value_size_bytes = &self.data[self.offset..(self.offset + 4)];
-            let value_size = Entry::parse_size(value_size_bytes);
-            self.offset += 4;
-
-            let key = str::from_utf8(&self.data[self.offset..(self.offset + key_size)]).ok()?;
-            self.offset += key_size;
-            let value = str::from_utf8(&self.data[self.offset..(self.offset + value_size)]).ok()?;
-            self.offset += value_size;
-
-            if !is_deleted {
-                return Some(Entry {
-                    offset: start,
-                    key: key.to_owned(),
-                    value: value.to_owned(),
-                });
+            match Entry::from_bytes(&self.data, start) {
+                Some(x) => {
+                    self.offset += x.len;
+                    if x.live {
+                        return Some(x);
+                    } else {
+                        continue;
+                    }
+                }
+                None => continue,
             }
         }
         return None;

eca22fc950b1bc314dbdcd86e4c57793556a8aa6
Daniel Velasquez Neira <danivelas4@gmail.com>
2025-11-04 13:23:26 -0500

    removed some duplication M TODO.md M src/disk/reader.rs

--- TODO.md
+++ TODO.md
@@ -19,9 +19,10 @@
 - [x] probably encapsulate file reading in its own class
 - [x] make sure that all cases of `libc::write` use the `safe_` equivalents in server.rs
 - [x] make sure that all cases of `libc::read` use the `safe_` equivalents in server.rs
-- [ ] make signal logic cleaner in `main.rs`
-- [ ] remove duplication of logic in disk/reader.rs when reading the key-value sizes
-- [ ] remove duplication of logic in `append_key` in disk/map.rs when creating key-value pairs
+- [x] make signal logic cleaner in `main.rs`
+- [x] remove duplication of logic in disk/reader.rs when reading the key-value sizes
+- [x] remove duplication of logic in `append_key` in disk/map.rs when creating key-value pairs
+- [ ] add Entry::from_bytes function
 - [ ] remove duplication of logic in disk/map.rs of pattern of open+lock+someop+unlock
 
 ## subjective
--- src/disk/reader.rs
+++ src/disk/reader.rs
@@ -16,20 +16,9 @@ impl Entry {
     }
 
     pub fn to_bytes(&self) -> Result<Vec<u8>, Box<dyn error::Error>> {
-        let klen: u32 = self.key.len().try_into()?;
-        let key_size_bytes: [u8; 4] = [
-            ((klen >> 24) as u8),
-            ((klen >> 16) as u8),
-            ((klen >> 8) as u8),
-            (klen as u8),
-        ];
-        let vlen: u32 = self.value.len().try_into()?;
-        let value_size_bytes: [u8; 4] = [
-            ((vlen >> 24) as u8),
-            ((vlen >> 16) as u8),
-            ((vlen >> 8) as u8),
-            (vlen as u8),
-        ];
+        let key_size_bytes = Entry::size_to_bytes(self.key.len().try_into()?);
+        let value_size_bytes = Entry::size_to_bytes(self.value.len().try_into()?);
+
         let mut buf = Vec::<u8>::with_capacity(
             1 + key_size_bytes.len() + value_size_bytes.len() + self.key.len() + self.value.len(),
         );
@@ -40,6 +29,22 @@ impl Entry {
         buf.extend_from_slice(self.value.as_bytes());
 
         Ok(buf)
+    }
+
+    fn size_to_bytes(size: u32) -> [u8; 4] {
+        [
+            ((size >> 24) as u8),
+            ((size >> 16) as u8),
+            ((size >> 8) as u8),
+            (size as u8),
+        ]
+    }
+
+    fn parse_size(bytes: &[u8]) -> usize {
+        (((bytes[0] as i32) << 24)
+            | ((bytes[1] as i32) << 16)
+            | ((bytes[2] as i32) << 8)
+            | (bytes[3] as i32)) as usize
     }
 }
 
@@ -69,18 +74,12 @@ impl Iterator for ReadResult {
             // get key size field, it is 4 bytes long and stored in big-endian
             // if number is 0xCAFEBABE, it is stored as CA FE BA BE
             let key_size_bytes = &self.data[self.offset..(self.offset + 4)];
-            let key_size = (((key_size_bytes[0] as i32) << 24)
-                | ((key_size_bytes[1] as i32) << 16)
-                | ((key_size_bytes[2] as i32) << 8)
-                | (key_size_bytes[3] as i32)) as usize;
+            let key_size = Entry::parse_size(key_size_bytes);
             self.offset += 4;
 
             // get value size field, also 4 bytes long and stored in big-endian
             let value_size_bytes = &self.data[self.offset..(self.offset + 4)];
-            let value_size = (((value_size_bytes[0] as i32) << 24)
-                | ((value_size_bytes[1] as i32) << 16)
-                | ((value_size_bytes[2] as i32) << 8)
-                | (value_size_bytes[3] as i32)) as usize;
+            let value_size = Entry::parse_size(value_size_bytes);
             self.offset += 4;
 
             let key = str::from_utf8(&self.data[self.offset..(self.offset + key_size)]).ok()?;
